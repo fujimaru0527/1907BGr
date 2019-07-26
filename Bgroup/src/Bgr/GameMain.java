f731a7066e66f08cf048ff83bdf05ad57b90c007
kawakami-masachika <43665352+kawakami-masachika@users.noreply.github.com>
2019-07-26 10:32:05 +0900

    メインクラス作成
    フレーム作成

--- Bgroup/src/Bgr/GameMain.java
+++ Bgroup/src/Bgr/GameMain.java
@@ -1,10 +1,20 @@
 package Bgr;
 
+import java.awt.Panel;
+
+import javax.swing.JFrame;
+
 public class GameMain {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-
+		JFrame jframe  = new JFrame();
+		jframe.setSize(800,600);
+        jframe.setLocationRelativeTo(null);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.setResizable(false);
+        Panel.createPanel(frame);
+        jframe.setVisible(true);
 	}
 
 }
